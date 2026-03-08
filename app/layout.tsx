c73714f4b96a965f0f00d9beb9d4acc07e8436f0
n0wherefast <97000096+n0wherefast@users.noreply.github.com>
2026-03-08 16:33:47 +0100

    new-GTAG

--- app/layout.tsx
+++ app/layout.tsx
@@ -75,7 +75,7 @@ export default function RootLayout({
       <head>
         <JsonLd />
         {/* <GoogleTagManager gtmId="GTM-NLXNKSM6" /> */}
-        {/* <GoogleAnalytics gaId="G-29BMJ1PT5L" /> */}
+        <GoogleAnalytics gaId="G-29BMJ1PT5L" />
         <Script id="gtm-psvmzxv8" strategy="afterInteractive" dangerouslySetInnerHTML={{ __html: `
           (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
           new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
