9898e03fb5b12173208f19fa51206975cf316e7c
n0wherefast <97000096+n0wherefast@users.noreply.github.com>
2026-03-17 17:27:40 +0100

    new-GTAG

--- app/layout.tsx
+++ app/layout.tsx
@@ -76,18 +76,18 @@ export default function RootLayout({
         <JsonLd />
         {/* <GoogleTagManager gtmId="GTM-NLXNKSM6" /> */}
         <GoogleAnalytics gaId="G-29BMJ1PT5L" />
-        <Script id="gtm-psvmzxv8" strategy="afterInteractive" dangerouslySetInnerHTML={{ __html: `
+        <Script id="GTM-NLXNKSM6" strategy="afterInteractive" dangerouslySetInnerHTML={{ __html: `
           (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
           new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
           j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-PSVMZXV8');
+          })(window,document,'script','dataLayer','GTM-NLXNKSM6');
         `}} />
       </head>
       <body className={inter.className}>
         <noscript>
           <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-PSVMZXV8"
+            src="https://www.googletagmanager.com/ns.html?id=GTM-NLXNKSM6"
             height="0"
             width="0"
             style={{ display: 'none', visibility: 'hidden' }}
